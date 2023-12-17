9ae49a3ebffb4d9d81b33906d5ea9b328017872a
Angel Pereira Diniz <127263320+AngelDiniz@users.noreply.github.com>
2023-12-17 20:02:17 -0300

    Delete Nova pasta/teste.py directory
    delete teste

--- Nova pasta/teste.py/aulamedia.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input('Digite aprimeira nota: '))
-n2 = int(input('Digite a segunda nota'))
-media = (n1 + n2 )/ 2
-print(f'A sua media foi {media}')
-if media <= 5:
-    print('A sua nota ta ruim')
-else:
-    print('A sua nota foi otima!')
-    
